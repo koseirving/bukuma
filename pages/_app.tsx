a41b8cb701acaaa6a7aaa759659fb095f21ffa4a
haduki <koseirving@gmail.com>
2023-02-05 21:42:55 +0900

    redirect unauthenticated users to the login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import Header from '../components/Header';
+import AuthWrapper from '../components/AuthWrapper';
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
+    <SessionProvider session={pageProps.session}>
+      <AuthWrapper>
+        <Header />
+        <Component {...pageProps} />
+      </AuthWrapper>
     </SessionProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
@@ -13,9 +14,9 @@ const Home: NextPage = () => {
       <main className='flex w-full flex-1 flex-col items-center justify-center px-20 text-center'>
         <h1 className='text-6xl font-bold'>
           Welcome to{' '}
-          <a className='text-blue-600' href='https://nextjs.org'>
-            Next.js!
-          </a>
+          <Link href='https://nextjs.org' legacyBehavior>
+            <a className='text-blue-600'>Next.js!</a>
+          </Link>
         </h1>
 
         <p className='mt-3 text-2xl'>
@@ -26,58 +27,64 @@ const Home: NextPage = () => {
         </p>
 
         <div className='mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full'>
-          <a
-            href='https://nextjs.org/docs'
-            className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
-          >
-            <h3 className='text-2xl font-bold'>Documentation &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+          <Link href='https://nextjs.org/docs' legacyBehavior>
+            <a className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'>
+              <h3 className='text-2xl font-bold'>Documentation &rarr;</h3>
+              <p className='mt-4 text-xl'>
+                Find in-depth information about Next.js features and its API.
+              </p>
+            </a>
+          </Link>
 
-          <a
-            href='https://nextjs.org/learn'
-            className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
-          >
-            <h3 className='text-2xl font-bold'>Learn &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+          <Link href='https://nextjs.org/learn' legacyBehavior>
+            <a className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'>
+              <h3 className='text-2xl font-bold'>Learn &rarr;</h3>
+              <p className='mt-4 text-xl'>
+                Learn about Next.js in an interactive course with quizzes!
+              </p>
+            </a>
+          </Link>
 
-          <a
+          <Link
             href='https://github.com/vercel/next.js/tree/canary/examples'
-            className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
+            legacyBehavior
           >
-            <h3 className='text-2xl font-bold'>Examples &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+            <a className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'>
+              <h3 className='text-2xl font-bold'>Examples &rarr;</h3>
+              <p className='mt-4 text-xl'>
+                Discover and deploy boilerplate example Next.js projects.
+              </p>
+            </a>
+          </Link>
 
-          <a
+          <Link
             href='https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
+            legacyBehavior
           >
-            <h3 className='text-2xl font-bold'>Deploy &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <a className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'>
+              <h3 className='text-2xl font-bold'>Deploy &rarr;</h3>
+              <p className='mt-4 text-xl'>
+                Instantly deploy your Next.js site to a public URL with Vercel.
+              </p>
+            </a>
+          </Link>
         </div>
       </main>
 
       <footer className='flex h-24 w-full items-center justify-center border-t'>
-        <a
-          className='flex items-center justify-center gap-2'
+        <Link
           href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
+          legacyBehavior
         >
-          Powered by{' '}
-          <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-        </a>
+          <a
+            className='flex items-center justify-center gap-2'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Powered by{' '}
+            <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
+          </a>
+        </Link>
       </footer>
     </div>
   );
